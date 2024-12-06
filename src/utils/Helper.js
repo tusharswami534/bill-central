d95e68be73c287357ff870b42b1427a96b628f02
tusharswami534 <tusharswami17@gmail.com>
2024-12-06 22:04:51 +0530

    add map data

--- src/utils/Helper.js
+++ src/utils/Helper.js
@@ -1,5 +1,11 @@
-import moduleName from '../assets/images/png/'
-
+import FIrstEnergy from '../assets/images/png/first-energy.png'
+import BlueNrg from '../assets/images/png/blue-nrg.png'
+import DoDo from '../assets/images/png/dodo.png'
+import Origin from '../assets/images/png/origin.png'
+import Ovo from '../assets/images/png/ovo.png'
+import Sumo from '../assets/images/png/sumo.png'
+import Alg from '../assets/images/png/agl.png'
+import { Bundle, ComprehensiveSupport, Electricity, GasPlan, InternetPlan, NoService, SaveTime, Simplified } from './Icons'
 export const HERDER_LIST = [
     {
         name: 'About',
@@ -28,3 +34,79 @@ export const HERDER_LIST = [
     },
 ]
 
+export const COMPANIES_LOGO = [
+        {
+            Logo : FIrstEnergy
+        },
+        {
+            Logo : BlueNrg
+        },
+        {
+            Logo : BlueNrg
+        },
+        {
+            Logo : BlueNrg
+        },
+        {
+            Logo : DoDo
+        },
+        {
+            Logo : Origin
+        },
+        {
+            Logo : Ovo
+        },
+        {
+            Logo : Sumo
+        },
+        {
+            Logo : Alg
+        },
+]
+
+export const COMPREHENSIVE_LIST = [
+    {
+        Icon : <Electricity/>,
+        Heading : 'Electricity Plan Comparisons',
+        Description : 'Whether you want renewable energy, low rates, or flexible contracts, we’ll help you find the best provider.'
+    },
+    {
+        Icon : <GasPlan/>,
+        Heading : 'Gas Plan Comparisons',
+        Description : 'Whether you want renewable energy, low rates, or flexible contracts, we’ll help you find the best provider.'
+    },
+    {
+        Icon : <InternetPlan/>,
+        Heading : 'Internet Plan Comparisons',
+        Description : 'Whether you want renewable energy, low rates, or flexible contracts, we’ll help you find the best provider.'
+    },
+    {
+        Icon : <Bundle/>,
+        Heading : 'Bundle Packages',
+        Description : 'Whether you want renewable energy, low rates, or flexible contracts, we’ll help you find the best provider.'
+    },
+]
+
+export const UTILITIES_LIST = [
+    {
+        Icon : <Simplified/>,
+        Heading : 'Simplified Comparison Process',
+        Description: 'Our intuitive platform lets you compare the best utility providers in one place, helping you make a quick and informed decision.'
+    },
+    {
+        Icon : <SaveTime/>,
+        Heading : 'Save Time and Money',
+        Description: 'With up-to-date plans and pricing, you can be sure you’re getting the most competitive rates available.'
+    },
+    {
+        Icon : <NoService/>,
+        Heading : 'No Service Interruptions',
+        Description: 'We take care of the entire switching process so you can enjoy a hassle-free experience with zero downtime.'
+    },
+    {
+        Icon : <ComprehensiveSupport/>,
+        Heading : 'Comprehensive Support',
+        Description: 'Whether you need assistance selecting a plan or during the switch, our dedicated support team is here to help every step of the way.'
+    },
+
+]
